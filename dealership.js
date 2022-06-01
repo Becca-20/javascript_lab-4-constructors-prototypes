9294214ebcd5c110ca9c88d0c6468cbe3efbcd8c
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-01 17:13:25 +0100

    count cars in stock method + exporting dealership constructor and count cars in stock method

--- dealership.js
+++ dealership.js
@@ -3,5 +3,20 @@
 const Dealership = function (name, maxCars, carsInStock) {
     this.name = name;
     this.maxCars = maxCars;
-    this.carsInStock = carsInStock;
+    this.carsInStock = [];
 }
+
+// export dealership constructor
+module.exports = {Dealership};
+
+
+
+// count no. of cars in stock
+Dealership.prototype.countCarsInStock = function () {
+    return this.carsInStock.length;
+}
+
+// export count cars in stock method
+module.exports = {countCarsInStock};
+
+

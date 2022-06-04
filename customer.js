b35706db175c70120735106bec2bb98b3c66084c
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-04 11:09:44 +0100

    customer constructor

--- customer.js
+++ customer.js
@@ -2,3 +2,9 @@
 // Give them a buyCar method which removes a Car from a Dealership and updates their car property.
 // Write tests to ensure all possible conditions are met, eg. the customer can afford the car.
 
+// customer constructor
+const Customer = function (name, wallet, car) {
+    this.name = name;
+    this.wallet = wallet;
+    this.car = null;
+}

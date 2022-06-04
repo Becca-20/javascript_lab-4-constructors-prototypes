d2f6542a5b073b66cdc4489791c581c4175646f8
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-04 23:21:08 +0100

    wrote + tested customer constructor

--- customer.test.js
+++ customer.test.js
@@ -1,3 +1,16 @@
 // import from Customer class
+const { Car } = require('./car');
 const {Customer} = require ('./customer');
 
+// access to constructor properties
+test('can access customer properties', () => {
+    
+    // arrange
+    let customer1 = new Customer ("Jim", 500);
+
+    // assert
+    expect(customer1.name).toBe("Jim");
+    expect(customer1.wallet).toBe(500);
+    expect(customer1.car).toBe(null);
+
+})

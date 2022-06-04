9f7587e2ea3d4e41e39752024622401f201fa64b
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-04 23:37:24 +0100

    wrote + tested can buy car

--- customer.js
+++ customer.js
@@ -1,4 +1,3 @@
-// Add a Customer class with a name, a wallet representing the amount of money they have and a car property which is initialised to be null.
 // Give them a buyCar method which removes a Car from a Dealership and updates their car property.
 // Write tests to ensure all possible conditions are met, eg. the customer can afford the car.
 
@@ -6,7 +5,18 @@
 const Customer = function (name, wallet, car) {
     this.name = name;
     this.wallet = wallet;
-    this.car = null;
+    this.car = [];
 }
 
 module.exports = {Customer};
+
+
+// buy car method - removes a Car from a Dealership + updates their car property
+Customer.prototype.buyCar = function (car) {
+    if (this.Car <= this.wallet && this.Dealership != null) {
+        this.Car = car;
+        this.wallet -= car.price;
+        this.Dealership.remove(Car);
+    }
+    this.car.push(car);
+}
--- customer.test.js
+++ customer.test.js
@@ -1,6 +1,7 @@
 // import from Customer class
 const { Car } = require('./car');
 const {Customer} = require ('./customer');
+const { Dealership } = require('./dealership');
 
 // access to constructor properties
 test('can access customer properties', () => {
@@ -11,6 +12,24 @@ test('can access customer properties', () => {
     // assert
     expect(customer1.name).toBe("Jim");
     expect(customer1.wallet).toBe(500);
-    expect(customer1.car).toBe(null);
+    expect(customer1.car.length).toBe(0);
 
+})
+
+
+describe('buy car', () => {
+
+    test('CAN buy car', () => {
+
+        // arrange
+        let customer2 = new Customer ("Pam", 9000);
+
+        let dealership8 = new Dealership ("Dwight's Wheels", 12, [new Car ("Fiat", 100, "F2"), new Car ("Honda", 50, "H3"), new Car ("Range Rover", 8365, "R90"), new Car ("Fiat", 839, "F5")]);
+
+        // act
+        customer2.buyCar("Fiat");
+
+        // assert
+        expect(customer2.car.length).toBe(1);
+    })
 })

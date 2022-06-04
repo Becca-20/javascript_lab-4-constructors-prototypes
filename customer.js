7c1cea42469880d218f5bf0505c4f3cc42f39bbb
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-04 11:04:04 +0100

    made customer file

---
+++ customer.js
@@ -0,0 +1,4 @@
+// Add a Customer class with a name, a wallet representing the amount of money they have and a car property which is initialised to be null.
+// Give them a buyCar method which removes a Car from a Dealership and updates their car property.
+// Write tests to ensure all possible conditions are met, eg. the customer can afford the car.
+

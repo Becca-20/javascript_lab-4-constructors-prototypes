77940489dc640d57dde053d82a75d2f143fcc98a
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-06 07:43:41 +0100

    stuck on customer buy car method

--- customer.js
+++ customer.js
@@ -1,22 +1,27 @@
-// Give them a buyCar method which removes a Car from a Dealership and updates their car property.
+// import car
+const {Car} = require('./car');
+
+
 // Write tests to ensure all possible conditions are met, eg. the customer can afford the car.
 
 // customer constructor
 const Customer = function (name, wallet, car) {
     this.name = name;
     this.wallet = wallet;
-    this.car = [];
+    this.cars = [];
 }
 
 module.exports = {Customer};
 
 
 // buy car method - removes a Car from a Dealership + updates their car property
-Customer.prototype.buyCar = function (car) {
-    if (this.Car <= this.wallet && this.Dealership != null) {
-        this.Car = car;
-        this.wallet -= car.price;
+Customer.prototype.buyCar = function (cars) {
+    if (Car.price <= this.wallet && this.Dealership != null) {
+        this.cars = cars;
+        this.wallet -= Car.price;
         this.Dealership.remove(Car);
+        this.cars.push(Car); 
+    } else {
+        console.log(`Sorry, you cannot make this purchase!`);
     }
-    this.car.push(car);
 }
--- customer.test.js
+++ customer.test.js
@@ -12,7 +12,7 @@ test('can access customer properties', () => {
     // assert
     expect(customer1.name).toBe("Jim");
     expect(customer1.wallet).toBe(500);
-    expect(customer1.car.length).toBe(0);
+    expect(customer1.cars.length).toBe(0);
 
 })
 
@@ -27,9 +27,25 @@ describe('buy car', () => {
         let dealership8 = new Dealership ("Dwight's Wheels", 12, [new Car ("Fiat", 100, "F2"), new Car ("Honda", 50, "H3"), new Car ("Range Rover", 8365, "R90"), new Car ("Fiat", 839, "F5")]);
 
         // act
-        customer2.buyCar("Fiat");
+        customer2.buyCar(dealership8.carsInStock[1]);
 
         // assert
-        expect(customer2.car.length).toBe(1);
+        // expect(customer2.car.length).toBe(1);
+        expect(customer2.cars.length).toBe(1);
     })
+
+    // test('CANNOT buy car', () => {
+
+    //     // arrange
+    //     let customer3 = new Customer ("Kevin", 10);
+
+    //     let dealership9 = new Dealership ("Angela's Cars", 10, [new Car ("Fiat", 100, "F2"), new Car ("Honda", 50, "H3"), new Car ("Range Rover", 8365, "R90"), new Car ("Fiat", 839, "F5")]);
+
+    //     // act
+    //     customer3.buyCar("Honda");
+
+    //     // assert
+    //     expect(customer3.car.length).toBe(0);
+    // })
+
 })
